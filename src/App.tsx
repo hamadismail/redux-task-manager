5cb7c4425a327746c2419059fc0a593b12492437
Hamad Ismail <hamad.ismail.gub@gmail.com>
2025-06-29 20:08:56 +0600

    project setup with shadcn and theme toggler

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { ModeToggle } from "./components/mode-toggle";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <Button>Click me</Button>
+      <ModeToggle />
+    </>
+  );
+}
+
+export default App;
